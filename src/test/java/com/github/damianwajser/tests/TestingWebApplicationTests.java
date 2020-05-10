286bdad06b701d1ff945ac774b1b2a7f97fa6618
damian wajser <damianwajser@damiPc.local>
2020-05-10 19:14:19 -0300

    add test

--- src/test/java/com/github/damianwajser/tests/TestingWebApplicationTests.java
+++ src/test/java/com/github/damianwajser/tests/TestingWebApplicationTests.java
@@ -4,7 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -13,8 +14,10 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class TestingWebApplicationTests {
 	@LocalServerPort
 	private int port;
@@ -22,29 +25,29 @@ public class TestingWebApplicationTests {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
-//	@Test
-//	public void testOK() throws Exception {
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.set("X-Client-Id", "3");
-//
-//		HttpEntity<String> entity = new HttpEntity<>("body", headers);
-//
-//		Map<String, Map<String, String>> response = this.restTemplate
-//				.exchange("http://localhost:" + port + "/replayheaders", HttpMethod.GET, entity, Map.class).getBody();
-//		assertThat(response.get("headers").get("X-Client-Id")).contains("3");
-//	}
-//
-//	@Test
-//	public void testNOK() throws Exception {
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.set("X-Client-Id", "4");
-//
-//		HttpEntity<String> entity = new HttpEntity<>("body", headers);
-//
-//		Map<String, Map<String, String>> response = this.restTemplate
-//				.exchange("http://localhost:" + port + "/replayheaders", HttpMethod.GET, entity, Map.class).getBody();
-//		assertThat(response.get("headers").get("X-Client-Id")).doesNotContain("3");
-//	}
+	@Test
+	public void testOK() throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("X-Client-Id", "3");
+
+		HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+		Map<String, Map<String, String>> response = this.restTemplate
+				.exchange("http://localhost:" + port + "/replayheaders", HttpMethod.GET, entity, Map.class).getBody();
+		assertThat(response.get("headers").get("X-Client-Id")).contains("3");
+	}
+
+	@Test
+	public void testNOK() throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("X-Client-Id", "4");
+
+		HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+		Map<String, Map<String, String>> response = this.restTemplate
+				.exchange("http://localhost:" + port + "/replayheaders", HttpMethod.GET, entity, Map.class).getBody();
+		assertThat(response.get("headers").get("X-Client-Id")).doesNotContain("3");
+	}
 
 }
 //@SpringBootTest
