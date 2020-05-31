25b6307dea0e3bcca8ea987e63aec066063bf8a5
Eliana88 <eliana.ramos1988@gmail.com>
2020-05-31 19:39:09 -0300

    excepciones prueba

---
+++ spring-commons-exception/src/main/java/com/github/damianwajser/exceptions/impl/methodnotallowed/MethodNotAllowedException.java
@@ -0,0 +1,31 @@
+package com.github.damianwajser.exceptions.impl.methodnotallowed;
+
+import com.github.damianwajser.exceptions.RestException;
+import com.github.damianwajser.exceptions.model.ExceptionDetail;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+@ResponseStatus(code = HttpStatus.METHOD_NOT_ALLOWED)
+public class MethodNotAllowedException extends RestException {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8012762310079534874L;
+
+	public MethodNotAllowedException(ExceptionDetail detail) {
+		super(detail);
+	}
+
+	public MethodNotAllowedException(List<ExceptionDetail> details) {
+		super(details);
+	}
+
+	public MethodNotAllowedException(String errorCode, String errorMessage, Optional<Object> errorDetail) {
+		super(errorCode, errorMessage, errorDetail);
+	}
+}
---
+++ spring-commons-exception/src/main/java/com/github/damianwajser/exceptions/impl/notacceptable/NotAcceptableException.java
@@ -0,0 +1,31 @@
+package com.github.damianwajser.exceptions.impl.notacceptable;
+
+import com.github.damianwajser.exceptions.RestException;
+import com.github.damianwajser.exceptions.model.ExceptionDetail;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE)
+public class NotAcceptableException extends RestException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8012762310079534874L;
+
+	
+	public NotAcceptableException(ExceptionDetail detail) {
+		super(detail);
+	}
+
+	public NotAcceptableException(List<ExceptionDetail> details) {
+		super(details);
+	}
+
+	public NotAcceptableException(String errorCode, String errorMessage, Optional<Object> errorDetail) {
+		super(errorCode, errorMessage, errorDetail);
+	}
+}
