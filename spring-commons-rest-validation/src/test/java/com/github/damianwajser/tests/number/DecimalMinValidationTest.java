b62226f43847e5e7d8d42d10694e9fc1ee690abb
damian wajser <wajser@gmail.com>
2020-08-27 17:11:07 -0300

    fix snake case problem

--- spring-commons-rest-validation/src/test/java/com/github/damianwajser/tests/number/DecimalMinValidationTest.java
+++ spring-commons-rest-validation/src/test/java/com/github/damianwajser/tests/number/DecimalMinValidationTest.java
@@ -1,7 +1,7 @@
 package com.github.damianwajser.tests.number;
 
-import com.github.damianwajser.model.number.DecimalMinObject;
 import com.github.damianwajser.model.number.DecimalMinInclusiveObject;
+import com.github.damianwajser.model.number.DecimalMinObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
