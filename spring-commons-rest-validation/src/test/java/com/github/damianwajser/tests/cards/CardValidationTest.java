528e34ac8a7dc71b06f8be2351a204818ac130f8
luisalberto-morales <luisalberto.morales@globant.com>
2020-11-04 11:09:25 -0600

    isValidExpirable was fixed in order to valid correct month-year

--- spring-commons-rest-validation/src/test/java/com/github/damianwajser/tests/cards/CardValidationTest.java
+++ spring-commons-rest-validation/src/test/java/com/github/damianwajser/tests/cards/CardValidationTest.java
@@ -25,8 +25,9 @@ public class CardValidationTest {
 	public void expirationMonth() throws Exception {
 		assertThat(validationFor(new ExpirationObject()), fails());
 		assertThat(validationFor(new ExpirationObject(10, 30)), succedes());
+		assertThat(validationFor(new ExpirationObject(11, 26)), succedes());
 		assertThat(validationFor(new ExpirationObject(2, 19)), fails());
-
+		assertThat(validationFor(new ExpirationObject(99, 25)), fails());
 	}
 
 }
