7bb1d3e438c2965b01349e28a9b9efc8c34f0262
luisalberto-morales <luisalberto.morales@globant.com>
2020-11-04 11:42:46 -0600

    Fixed isValidExpirable, added new test

--- spring-commons-rest-validation/src/test/java/com/github/damianwajser/tests/cards/CardValidationTest.java
+++ spring-commons-rest-validation/src/test/java/com/github/damianwajser/tests/cards/CardValidationTest.java
@@ -26,6 +26,7 @@ public class CardValidationTest {
 		assertThat(validationFor(new ExpirationObject()), fails());
 		assertThat(validationFor(new ExpirationObject(10, 30)), succedes());
 		assertThat(validationFor(new ExpirationObject(11, 26)), succedes());
+		assertThat(validationFor(new ExpirationObject(5, 25)), succedes());
 		assertThat(validationFor(new ExpirationObject(2, 19)), fails());
 		assertThat(validationFor(new ExpirationObject(99, 25)), fails());
 	}
