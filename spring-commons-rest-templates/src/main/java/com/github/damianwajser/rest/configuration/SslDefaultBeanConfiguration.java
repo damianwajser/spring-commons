8a7cd3d18acce9106192302538c5643fc5a5f884
damian wajser <wajser@gmail.com>
2020-11-25 09:58:51 -0300

    sonar issues

--- spring-commons-rest-templates/src/main/java/com/github/damianwajser/rest/configuration/SslDefaultBeanConfiguration.java
+++ spring-commons-rest-templates/src/main/java/com/github/damianwajser/rest/configuration/SslDefaultBeanConfiguration.java
@@ -1,7 +1,6 @@
 package com.github.damianwajser.rest.configuration;
 
 import org.apache.http.ssl.SSLContextBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
