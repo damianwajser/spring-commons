a3fbfc4b6ae7d9c293f10e54cd9d3cea377beaf9
damian wajser <damian.wajser@globant.com>
2020-12-15 12:47:56 -0300

    add pool manager in restTemplate

--- spring-commons-exception-handler/src/test/java/com/github/damianwajser/controllers/OtheRequestController.java
+++ spring-commons-exception-handler/src/test/java/com/github/damianwajser/controllers/OtheRequestController.java
@@ -2,15 +2,9 @@ package com.github.damianwajser.controllers;
 
 import com.github.damianwajser.exceptions.impl.badrequest.BadRequestException;
 import com.github.damianwajser.model.FooObject;
-import com.github.damianwajser.validator.annotation.global.Min;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.util.Optional;
 
 @RestController
 @Validated
